dc10eee86b8bd25876dd46656988b48ae84e9e45
Sumana Chakrabarti <sumanac@outlook.com>
2018-03-05 15:00:13 -0800

    changes for test setup

---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import  fs  from 'fs';
+import jsdom from 'jsdom';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, function(err, window){
+      // const h1 = window.document.getElementsBytagName('h1')[0];
+      console.log("there have been", window);
+      // expect(h1.innerHTML).to.equal("Hello World");
+      done();
+      window.close();
+    });
+  });
+})
